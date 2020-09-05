2efb97342fd4a88223680773f267b15fcc4d7907
joyjit <das.joyjit@tcs.com>
2020-09-05 15:21:30 +0530

    add location service

---
+++ src/location/src/test/java/com/estore/location/LocationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estore.location;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
